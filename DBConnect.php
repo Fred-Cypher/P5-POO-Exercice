f3bc386512135bef4a87897f0294642a082c8e79
Fred <frederique.perrier@gmail.com>
2024-10-22 13:55:26 +0200

    Add DBConnect, .gitignore and .env

---
+++ DBConnect.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class DBConnect{
+
+    private static $instance = null;
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = new PDO('mysql:host=' . $_ENV["DB_HOST"] .';dbname=' . $_ENV["DB_NAME"], "DB_USER", "DB_PASSWORD");
+    }
+  
+    public static function getInstance(): DBConnect
+    {
+        if (self::$instance == null) {
+            self::$instance = new DBConnect();
+        }
+        return self::$instance;
+    }
+
+    public function getPDO()
+    {
+        return $this->pdo;
+    }
+} 
